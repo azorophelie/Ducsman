4eae57a100f194edd3488267f1979ad7ca324ca3
azorophelie <azor.ophelie@yahoo.fr>
2025-09-03 23:22:09 +0200

    Site web hôtel

---
+++ ressourcesDucsman/slider.js
@@ -0,0 +1,11 @@
+const slide = ["images/slider/2 r", "images/slider/3"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slider2").src = slide[Number];
+}
